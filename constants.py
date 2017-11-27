802d249d2b50c42fcf7c48c02e5c6ef3fbb89a08
kpijanow <karolpijanowski@op.pl>
2017-11-27 20:23:47 +0100

    poprawione constatnts i zrobiony readout, w readoucie moze sam odczyt powinien byc w procesie?

--- constants.py
+++ constants.py
@@ -1,29 +1,31 @@
 class Constants:
-    cable_length = 15
-    det_X = calbelength * [1, 1, -1, -1]
-    det_Y = cable_length * [1, -1, -1, 1]
-    det_area = 0.31 * 0.27
-    readOut_eff = 0.95
-    det_eff = 0.75
+    
+    def __init__(self):
+        self.cable_length = 15
+        self.det_X = self.calbelength * [1, 1, -1, -1]
+        self.det_Y = self.cable_length * [1, -1, -1, 1]
+        self.det_area = 0.31 * 0.27
+        self.readOut_eff = 0.95
+        self.det_eff = 0.75
 
-    getDet_X():
-        return det_X
+    def getDet_X(self):
+        return self.det_X
 
-    getDet_Y():
-        return det_Y
+    def getDet_Y(self):
+        return self.det_Y
 
-    getDet_area():
-        return det_area
+    def getDet_area(self):
+        return self.det_area
 
-    getDet_eff():
-        return det_eff
+    def getDet_eff(self):
+        return self.det_eff
 
-    getReadOut_eff():
-        return readOut_eff
+    def getReadOut_eff(self):
+        return self.readOut_eff
 
-    setDet_X(det_X0):
-        det_X = det_X0
+    def setDet_X(self, det_X0):
+        self.det_X = det_X0
 
-    setDet_Y(det_Y0):
-        det_Y = det_Y0
+    def setDet_Y(self, det_Y0):
+        self.det_Y = det_Y0
     
--- readout.py
+++ readout.py
@@ -2,89 +2,86 @@ import serial as serial
 import binascii
 
 class ReadOut():
-    line = b''
-    new = False
-    events = list()
     
-    def get2Bytes(n):
-        global line
-	return bin(int(line[n:n+2].decode("utf8"), 16))[2:].zfill(8)
+    def __init__(self):
+        self.line = b''
+        self.new = False
+        self.events = list()
+        self.t1 = [0, 0, 0, 0]
+        self.t2 = [0, 0, 0, 0]
+    
+    def get2Bytes(self, n):
+        return bin(int(self.line[n:n+2].decode("utf8"), 16))[2:].zfill(8)
 
-    def checkIfNewEvent():
-	bits = get2Bytes(9)
-	if bits[0] == '1':
-	    return True
-	else:
-	    return False
+    def checkIfNewEvent(self):
+        bits = self.get2Bytes(9)
+        if bits[0] == '1':
+            return True
+        else:
+            return False
 
-    def checkIfGoodData(i):
-	bits = get2Bytes(9 + i * 3)
-	if bits[2] == '1':
-	    return True
-	else:
-	    return False
+    def checkIfGoodData(self, i):
+        bits = self.get2Bytes(9 + i * 3)
+        if bits[2] == '1':
+            return True
+        else:
+            return False
 
-    def getTimeTicks():
-        global line
-	for i in range(4):
-	    if checkIfGoodData(i * 2):
-		bits = get2Bytes(9 + i * 6)
-		t1[i] = int(bits[3:], 2)
-	    else:
-                t1[i] = -1
-                
-            if checkIfGoodData(line, i * 2 + 1):
-		bits = get2Bytes(12 + i * 6)
-		t2[i] = int(bits[3:], 2)
-	    else:
-                t1[i] = -1
+    def getTimeTicks(self):
+        for i in range(4):
+            if self.checkIfGoodData(i * 2):
+                bits = self.get2Bytes(9 + i * 6)
+                self.t1[i] = int(bits[3:], 2)    
+            else:
+                self.t1[i] = -1
+            
+            if self.checkIfGoodData(self.line, i * 2 + 1):
+                bits = self.get2Bytes(12 + i * 6)
+                self.t2[i] = int(bits[3:], 2)
+            else:
+                self.t1[i] = -1
 
-    def timeTicksToNanoS():
-	self.t1[:] = [x*5/4.0 for x in self.t1]
-	self.t2[:] = [x*5/4.0 for x in self.t2]
+    def timeTicksToNanoS(self):
+        self.t1[:] = [x*5/4.0 for x in self.t1]
+        self.t2[:] = [x*5/4.0 for x in self.t2]
 
-    def checkIfGoodReadout():
-	for i in range(4):
-            if t1[i] >= 0:
-		if (t2[i] >= 0) and (t2[i] < t1[i]):
-                    t2[i] += 40
+    def checkIfGoodReadout(self):
+        for i in range(4):
+            if self.t1[i] >= 0:
+                if (self.t2[i] >= 0) and (self.t2[i] < self.t1[i]):
+                    self.t2[i] += 40
 
-    def updateEvents():
+    def updateEvents(self):
         t = [0, 0, 0, 0, 0, 0, 0, 0]
         t[1:5] = self.t1
         t[4:] = self.t2
         self.events.append(t)
 
-    def readLine(line):
-        ser.close
-        line = ser.readline()
-        if line != b'':
-        readLine(line)
-	
-	if len(line) == 74:
-	    if checkIfNewEvent(line):
+    def readLine(self):
+        if len(self.line) == 74:
+            if self.checkIfNewEvent():
                 self.new = True
-		timeTicksToNanoS()
-		checkIfGoodReadout()
-		updateEvents()
-					
-		self.t1[:] = [0 for x in t1]
-		self.t2[:] = [0 for x in t2]
-		getTimeTicks(line)
+                self.timeTicksToNanoS()
+                self.checkIfGoodReadout()
+                self.updateEvents()
+        
+                self.t1[:] = [0 for x in self.t1]
+                self.t2[:] = [0 for x in self.t2]
+                self.getTimeTicks(self.line)
 		
             else:
-                getTimeTicks(line)
+                self.getTimeTicks(self.line)
 
-    def readLoop:
+    def readLoop(self):
         ser = serial.Serial('/dev/ttyUSB0', baudrate = 115200, bytesize = 8, parity = 'N', stopbits = 1, xonxoff = True, timeout = 0)
         while 1:
             ser.close
-            line = ser.readline()
-            if line != b'':
-                readLine(line)
+            self.line = ser.readline()
+            if self.line != b'':
+                self.readLine()
             
-    def getEvents():
-        events0 = events
-        events = []
+    def getEvents(self):
+        events0 = self.events
+        self.events = []
         return events0
             
