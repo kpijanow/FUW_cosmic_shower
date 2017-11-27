7145afa6f6892c32d7cb358b77eba633451fe5d8
krozwadowska <k.m.rozwadowska@gmail.com>
2017-11-27 20:23:27 +0100

    update

--- analize.py
+++ analize.py
@@ -9,8 +9,8 @@ detectedMuons = 0
 class Event(Constants):
 
     def __init__(self, line):
-        self.t1 = line[1:5]
-        self.t2 = line[4:]
+        self.t1 = np.array(line[0:4]) ## ??
+        self.t2 = np.array(line[4:])
         self.time = 0 
         #self.time = line[8]        # TO DO: add global time to readout.py
         self.ToT = [j-i for i,j in zip (self.t1, self.t2)]
@@ -33,28 +33,29 @@ class Event(Constants):
 
 ##----------------------------------------------
 ##    vectors start here and it's crap
-##----------------------------------------------
-    def getPoint(n):
-        rho = Constants.c*t1[n]
-        p = np.array([Constants.det_X[n], Constants.det_Y[n], 0])
-        v = p - p0
-        distance = math.sqrt(sum(j**2 for j in v))
-        theta = math.pi/2 - math.acos(rho/distance)
-        z = distance*math.tan(theta)
-        p = np.array([Constants.det_X[n], Constants.det_Y[n], z])
+##----------------------------------------------    
+    def getPoint(n):                                ## ooops
+##        rho = Constants.c*t1[n]
+##        p = np.array([Constants.det_X[n], Constants.det_Y[n], 0])
+##        v = p - p0
+##        distance = math.sqrt(sum(j**2 for j in v))
+##        theta = math.pi/2 - math.acos(rho/distance)
+##        z = distance*math.tan(theta)
+        p = np.array([Constants.det_X[n], Constants.det_Y[n], t1[n]])
+##        p = np.array([Constants.det_X[n], Constants.det_Y[n], z])
         return p
 
     def Direction(self):
         if self.nMuons < 3:
             return 0
         else:            
-            t = np.array(self.t1)           
-            t_min = np.argpartition(t,2)    ## where was the first hit -> this is our reference
-            if t[t_min[:2]][0]==0:
-                t_first = t[t_min[:2][1]]
+##            t = np.array(self.t1)           
+            t_min = np.argpartition(t1,2)    ## where was the first hit -> this is our reference
+            if t1[t_min[:2]][0]==0:
+                t_first = t1[t_min[:2][1]]
             else:
-                t_first = t[t_min[:2][0]]
-            [firstDet], = np.where(t==t_first)
+                t_first = t1[t_min[:2][0]]
+            [firstDet], = np.where(t1==t_first)
             p0 = np.array([Constants.det_X[firstDet], Constants.det_Y[firstDet], 0])
 ##            print(firstDet)
             detPoints = []                  ## detectors with next hits
@@ -69,12 +70,13 @@ class Event(Constants):
               n = detPoints[i]
               p[i][:] = getPoint(n)
 ##              print(p[i])          
-            v1 = p[1] - p0              ## I'd think these are in one plane, so cross prod would give (same) normal vector (it doesn't)
+            v1 = p[1] - p0              ## shiiiit
             v2 = p[2] - p0
             v3 = p[3] - p0
-            vector = np.cross(v1,v2)
-##            vector = np.cross(v1,v3)
-##            vector = np.cross(v2,v3)
+            cp1 = np.cross(v1,v2)
+            cp2 = np.cross(v1,v3)
+            cp3 = np.cross(v2,v3)
+            vector = cp1+cp2+cp3
             return vector
 ##-----------------------------------------
 ##-----------------------------------------
